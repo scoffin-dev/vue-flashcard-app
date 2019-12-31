f52ba8e55c018117f0d34ce443eef3aff5a7a3f6
scoffin-dev <tipsywithwater@gmail.com>
2019-12-31 18:05:28 -0500

    Polished functionality (tutorial guided)

--- app.js
+++ app.js
@@ -1,32 +1,58 @@
-const flashcardDeck = [
+const cards = [
     {
-        front: 'Term for a group of cats',
-        back: 'Clowder',
-        flipped: false
+      front: 'The "First Computer Programmer"',
+      back: 'Ada Lovelace',
+      flipped: false,
     },
     {
-        front: 'Term for a group of crows',
-        back: 'Murder',
-        flipped: false
+      front: 'Invented the "Clarke Calculator"',
+      back: 'Edith Clarke',
+      flipped: false,
+  
     },
     {
-        front: 'Term for a group of eagles',
-        back: 'Covocation',
-        flipped: false
-    }
-]
+      front: 'Famous World War II Enigma code breaker',
+      back: 'Alan Turing',
+      flipped: false,
+    },
+    {
+      front: 'Created satellite orbit analyzation software for NASA',
+      back: 'Dr. Evelyn Boyd Granville',
+      flipped: false,
+    },
+  ]; 
 
-new Vue({
+  // Get infor for new cards from user
+  // Add new card when user clicks a button
+  // Delete cards
+  // Animate card flip
+  // Display error message if front and/or back isn't provided for new card
+
+  new Vue({
     el: '#flashcard-app',
     data: {
-        deck: flashcardDeck
+      cards: cards,
+      newCardFront: '',
+      newCardBack: '',
+      error: false
     },
     methods: {
-        flip: function(card) {
-            card.flipped = !card.flipped;
+      toggleCard(card) {
+        card.flipped = !card.flipped;
+      },
+      addNewCard() {
+        if(!this.newCardFront || !this.newCardBack) {
+          this.error = true;
+        } else {
+          this.cards.push({
+            front: this.newCardFront,
+            back: this.newCardBack,
+            flipped: false
+          });
+          this.newCardFront = '';
+          this.newCardBack = '';
+          this.error = false;
         }
-    },
-    computed: {
-
+      }
     }
-});
+  });
