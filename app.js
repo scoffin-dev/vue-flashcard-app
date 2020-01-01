334247468ee8fae59c133445f9e8d66b51d6867d
scoffin-dev <tipsywithwater@gmail.com>
2019-12-31 19:45:24 -0500

    Setting mastery levels

--- app.js
+++ app.js
@@ -3,22 +3,33 @@ const cards = [
       front: 'The "First Computer Programmer"',
       back: 'Ada Lovelace',
       flipped: false,
+      totallyMastered: false,
+      almostMastered: false,
+      notMastered: true
     },
     {
       front: 'Invented the "Clarke Calculator"',
       back: 'Edith Clarke',
       flipped: false,
-  
+      totallyMastered: false,
+      almostMastered: false,
+      notMastered: true
     },
     {
       front: 'Famous World War II Enigma code breaker',
       back: 'Alan Turing',
       flipped: false,
+      totallyMastered: false,
+      almostMastered: false,
+      notMastered: true
     },
     {
       front: 'Created satellite orbit analyzation software for NASA',
       back: 'Dr. Evelyn Boyd Granville',
       flipped: false,
+      totallyMastered: false,
+      almostMastered: false,
+      notMastered: true
     },
   ]; 
 
@@ -33,6 +44,24 @@ const cards = [
     methods: {
       toggleCard(card) {
         card.flipped = !card.flipped;
+      },
+      setCardToTotallyMastered(card) {
+        card.flipped = !card.flipped;
+        card.totallyMastered = true;
+        card.almostMastered = false;
+        card.notMastered = false;
+      },
+      setCardToAlmostMastered(card) {
+        card.flipped = !card.flipped;
+        card.totallyMastered = false;
+        card.almostMastered = true;
+        card.notMastered = false;
+      },
+      setCardToNotMastered(card) {
+        card.flipped = !card.flipped;
+        card.totallyMastered = false;
+        card.almostMastered = false;
+        card.notMastered = true;
       },
       addNewCard() {
         if(!this.newCardFront || !this.newCardBack) {
